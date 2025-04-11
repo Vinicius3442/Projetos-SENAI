9745facece6fc46e9f6d6b4ef36f530c0e4b613c
Vinicius Montuani <viniciusmontuani@gmail.com>
2025-04-11 17:57:37 -0300

    adicionei arquivos .BAT criados no bloco de notas

---
+++ Outros códigos/.BAT/simpleloop.bat
@@ -0,0 +1,8 @@
+:loo
+start cmd
+goto loop
+REM CUIDADO
+REM Deixei o código errado para não executar sem querer, caso queira executa-lo adicione o "p" no ":loo" ficando ":loop".
+REM Esse código cria um loop infinito de janelas do cmd, o que pode travar o computador.
+REM Não execute este código sem saber o que está fazendo, foi uma luta para conseguir parar o loop.
+REM CUIDADO
