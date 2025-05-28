606b4e6d661ed049607983b13e55bcf46a518918
Vinicius Montuani <viniciusmontuani@gmail.com>
2025-05-28 15:57:19 -0300

    C# Updt

---
+++ Programação diversa/C#/CSharpBibliTeste/Program.cs
@@ -0,0 +1,44 @@
+﻿ //Teste Upper case
+string teste = "Rolagem de dados".ToUpper();
+
+Console.WriteLine(teste);
+Console.WriteLine();
+
+//Rolagem dado:
+
+Random dice = new Random();
+int rollD6 = dice.Next(1, 7);
+
+Console.WriteLine($"O número sorteado pelo D6 foi: {rollD6}");
+
+//Rolagem Tripla:
+
+int rollD8 = dice.Next(1, 9);
+int rollD12 = dice.Next(1, 13);
+int rollD20 = dice.Next(1, 21);
+
+Console.WriteLine($"O número sorteado pelo D8 foi: {rollD8}");
+Console.WriteLine($"O número sorteado pelo D12 foi: {rollD12}");
+Console.WriteLine($"O número sorteado pelo D20 foi: {rollD20}");
+
+//Bigger value desafio (minha solução):
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+if (firstValue > secondValue) {
+    largerValue = firstValue;
+} else {
+    largerValue = secondValue;
+}
+
+Console.WriteLine($"Minha solução {largerValue}");
+
+//Solução microsoft,
+
+int firstValue2 = 500;
+int secondValue2 = 600;
+int largerValue2;
+largerValue2 = Math.Max(firstValue2, secondValue2);
+Console.WriteLine($"Solução Microsoft learn: {largerValue2}");
---
+++ Programação diversa/C#/Temperature convert/Program.cs
@@ -0,0 +1,3 @@
+﻿int fahrenheit = 94;
+Decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.Write("A sua temperatura em Celsius é: " + celsius);
