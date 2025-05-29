eca05ae1a742191409b41460c49a3113937eff29
Vinicius Montuani <viniciusmontuani@gmail.com>
2025-05-29 20:02:12 -0300

    Atualizar o Program.cs

--- Programação diversa/C#/Serviço Expirado/Program.cs
+++ Programação diversa/C#/Serviço Expirado/Program.cs
@@ -1,25 +1,37 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+using System;
 
-if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+namespace SubscriptionReminder
 {
-    Console.Write("Your subscription will expire soon. Renew now!");
-}
+    class Program
+    {
+        public static void Main()
+        {
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12); // Gera um valor de 0 a 11
+            int discountPercentage = 0;
 
-else if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
-{
-    discountPercentage = 10;
-    Console.Write($"Your subscription expires in {daysUntilExpiration} days. \nRenew now and save {discountPercentage}%");
-}
+            Console.WriteLine($"Dias até expiração: {daysUntilExpiration}\n");
 
-else if (daysUntilExpiration == 1)
-{
-    discountPercentage = 20;
-    Console.Write($"Your subscription expires within a day! \nRenew now and save {discountPercentage}%!");
-}
-
-else if (daysUntilExpiration == 0)
-{
-    Console.Write($"Your subscription has expired.");
+            if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+            {
+                Console.WriteLine("Your subscription will expire soon. Renew now!");
+            }
+            else if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
+            {
+                discountPercentage = 10;
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+                Console.WriteLine($"Renew now and save {discountPercentage}%!");
+            }
+            else if (daysUntilExpiration == 1)
+            {
+                discountPercentage = 20;
+                Console.WriteLine("Your subscription expires within a day!");
+                Console.WriteLine($"Renew now and save {discountPercentage}%!");
+            }
+            else if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+        }
+    }
 }
