7a622194423d0bb9a7e9335781fee02719a0080e
Vinicius Montuani <viniciusmontuani@gmail.com>
2025-06-03 18:07:11 -0300

    redme img added

--- Programação diversa/C#/CSharpBibliTeste/obj/Debug/net8.0/CSharpBibliTeste.AssemblyInfo.cs
+++ Programação diversa/C#/CSharpBibliTeste/obj/Debug/net8.0/CSharpBibliTeste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpBibliTeste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13877feb1b4d5e124af948ff2b1b16366f67a87e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edfbd5e4755e072bba5ed5e226976492fdfed4b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpBibliTeste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpBibliTeste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
